2e63729e305af1e3cb9d18f239d590e094718fcb
Garima Mittal <garimamittal@Garimas-Air.attlocal.net>
2022-06-19 07:57:29 -0700

    namescore.js

--- namescore.js
+++ namescore.js
@@ -7,15 +7,26 @@
 
 const fs = require ('fs');
 
+const Alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 const nameScore = function(filename){
     const fileData = fs.readFileSync(filename, 'utf8');    
    // const nameArray = fileData.split(',');
    
    //replace global
    const nameArray = fileData.replace(/\"/g, '').split(',').sort();
-    console.log(fileData)
-    console.log(nameArray) // looks like names have single quote but they dont.
+    //console.log(fileData)
+    //console.log(nameArray) // looks like names have single quote but they dont.
     //it's just JS way of showing string
+
+
+    nameArray.forEach(firstName =>{
+        console.log(firstName)
+        firstName.split('').forEach(letter =>{
+            const letterPosition = Alpha.indexOf(letter) + 1;
+            console.log('\t', letter, letterPosition)
+        })
+    })
     return 5
 }
 
